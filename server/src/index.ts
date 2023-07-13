efe8845dc629b70cb4251ed25e70886e36ef246c
Huilen Solis <xywvanz@gmail.com>
2023-07-13 00:46:32 -0300

    changed imports exports

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,10 @@ import { dbSequelize } from './db';
 // middlewares
 import { logsMiddw } from './middlewares';
 
+// PORT
+const PORT = process.env.PORT || 3000;
+
+// app
 const app = express();
 app.use(express.json());
 app.use(logsMiddw);
@@ -13,13 +17,12 @@ app.use(logsMiddw);
     try {
         await dbSequelize.authenticate();
         console.log('Connection with databse has been established successfully.');
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
 })();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
