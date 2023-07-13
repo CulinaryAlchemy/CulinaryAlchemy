afe8c25e358b51f40dd57183d10dce60408c56c5
Huilen Solis <xywvanz@gmail.com>
2023-07-12 23:37:42 -0300

    generated db authentication

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+// libreries
+import express from 'express';
+
+// db
+import { dbSequelize } from './db';
+// routes
+
+const app = express();
+
+(async () => {
+    try {
+        await dbSequelize.authenticate();
+        console.log('Connection with databse has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})()
+
