4d1ea2df2712cdf1dbc1180c3d068e3aefbc7090
Huilen Solis <xywvanz@gmail.com>
2023-07-13 22:49:34 -0300

    fixed httpstatus codes object format

--- server/src/utils/http-status-code.ts
+++ server/src/utils/http-status-code.ts
@@ -1,35 +1,10 @@
 export const HttpStatusCodes = {
-    SUCCESS: {
-      name: 'Success',
-      value: 200,
-    },
-    CREATED: {
-      name: 'Created',
-      value: 201,
-    },
-    BAD_REQUEST: {
-      name: 'Bad Request',
-      value: 400,
-    },
-    UNAUTHORIZED: {
-      name: 'Unauthorized',
-      value: 401,
-    },
-    FORBIDDEN: {
-      name: 'Forbidden',
-      value: 403,
-    },
-    NOT_FOUND: {
-      name: 'Not Found',
-      value: 404,
-    },
-    METHOD_NOT_ALLOWED: {
-      name: 'Method Not Allowed',
-      value: 405,
-    },
-    INTERNAL_SERVER_ERROR: {
-      name: 'Internal Server Error',
-      value: 500,
-    },
-  };
-  
+  SUCCESS: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 403,
+  METHOD_NOT_ALLOWED: 405,
+  INTERNAL_SERVER_ERROR: 500,
+};
