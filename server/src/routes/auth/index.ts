07eed52ee0f6685a899374a6b68ea24f80a4dbeb
Huilen Solis <xywvanz@gmail.com>
2023-07-15 20:57:51 -0300

    started using email validator in register routes

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { register } from "./register";
 import { signIn } from "./sign-in";
+import { emailValidator } from "../../middlewares/valdiators";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", register);
-authRouter.post("/sign-in", signIn);
+authRouter.post("/register", emailValidator, register);
+authRouter.post("/sign-in", emailValidator, signIn);
 
 export { authRouter };
