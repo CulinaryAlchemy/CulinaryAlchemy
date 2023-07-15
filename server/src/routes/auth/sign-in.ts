db33b10853521b225764d1c4dbf2171103513c39
Huilen Solis <xywvanz@gmail.com>
2023-07-15 20:48:35 -0300

    created sign in function

---
+++ server/src/routes/auth/sign-in.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+
+import bcrypt from "bcrypt";
+import Jwt from "jsonwebtoken";
+import { UserProvider } from "../../providers/user";
+import { HttpStatusCodes, sendApiError, sendApiResponse } from "../../utils";
+
+export const signIn = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (!email && !password) {
+    sendApiError(
+      res,
+      HttpStatusCodes.BAD_REQUEST,
+      "Email and password are required"
+    );
+    return;
+  }
+
+  const user: any = await UserProvider.getUser.ByEmail(email);
+
+  if (!user || (await !bcrypt.compare(password, user.password))) {
+    sendApiError(res, HttpStatusCodes.NOT_FOUND, "bad credentials");
+    return;
+  }
+
+  const secret = process.env.JWT_SECRET || "secret";
+
+  const token = Jwt.sign({ sub: user.id }, secret, { expiresIn: "1h" });
+
+  sendApiResponse(res, HttpStatusCodes.SUCCESS, token);
+};
