f425136f3285946e2178f4820bbfb109bd1068f0
Huilen Solis <xywvanz@gmail.com>
2023-07-15 15:13:24 -0300

    hotfix: now id input comes in body and not in params

--- server/src/routes/user/get/by-id.ts
+++ server/src/routes/user/get/by-id.ts
@@ -5,7 +5,7 @@ import { HttpStatusCodes } from "../../../utils";
 import { Request, Response } from "express";
 
 export const getById = (req: Request, res: Response) => {
-  const { id } = req.params;
+  const { id } = req.body;
   try {
     UserProvider
       .getUser.ById(id)
