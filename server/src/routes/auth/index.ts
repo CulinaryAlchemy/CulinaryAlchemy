0e24d524094baba5741e2c3e8bfb398fa05ce745
Huilen Solis <xywvanz@gmail.com>
2023-07-15 20:49:49 -0300

    included sign in route

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { register } from "./register";
+import { signIn } from "./sign-in";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
+authRouter.post("/sign-in", signIn);
 
 export { authRouter };
