5b18cc038ba3a80209f84c9c466e8285585f3787
Jes015 <jes.18.sin.15@gmail.com>
2023-07-17 08:51:42 +0000

    feat(web-app): initialize app ui configuration

--- apps/web-app/src/App.tsx
+++ apps/web-app/src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@fontsource/public-sans'
+import CssBaseline from '@mui/joy/CssBaseline'
+import Typography from '@mui/joy/Typography'
+import { CssVarsProvider } from '@mui/joy/styles'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function App () {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <CssVarsProvider
+      defaultMode='dark'
+      colorSchemeSelector="#demo_dark-mode-by-default"
+      //
+      // the local storage key to use
+      modeStorageKey="demo_dark-mode-by-default"
+      //
+    >
+      <CssBaseline />
+      <div id="demo_dark-mode-by-default">
+        <Typography level='h1'>Initializing project</Typography>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </CssVarsProvider>
   )
 }
 
--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@mui/material': '@mui/joy'
+    }
+  }
 })
