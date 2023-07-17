b8898ddf30f8da42ba611703853d61a0fda5d6a7
Jes015 <jes.18.sin.15@gmail.com>
2023-07-17 11:00:43 +0000

    feat(web-app): add standard (linter rules)

--- apps/web-app/.eslintrc.cjs
+++ apps/web-app/.eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
+    'standard-with-typescript'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
