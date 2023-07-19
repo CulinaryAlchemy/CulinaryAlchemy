65108cb8f3e138412e4e65981a52188676bfc86d
Huilen Solis <xywvanz@gmail.com>
2023-07-18 23:21:31 -0300

    changed routes namis, from register to sign up

--- apps/server/src/routes/auth/index.ts
+++ apps/server/src/routes/auth/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { register } from "./register";
+import { register } from "./sign-up";
 import { signIn } from "./sign-in";
 import { emailValidator } from "../../middlewares/valdiators";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", emailValidator, register);
+authRouter.post("/sign-up", emailValidator, register);
 authRouter.post("/sign-in", emailValidator, signIn);
 
 export { authRouter };
