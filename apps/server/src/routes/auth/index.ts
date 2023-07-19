ad44218dee2eb59d422f363f40928f3fc8661f66
Huilen Solis <xywvanz@gmail.com>
2023-07-19 00:53:56 -0300

    changed register route name to sign-up

--- apps/server/src/routes/auth/index.ts
+++ apps/server/src/routes/auth/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { register } from "./register";
+import { register } from "./sign-up";
 import { signIn } from "./sign-in";
 import { emailValidator } from "../../middlewares/valdiators";
 
