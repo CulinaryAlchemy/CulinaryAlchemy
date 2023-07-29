4797beac5da90742c16e88dd7caae738d51e0324
Jes015 <jes.18.sin.15@gmail.com>
2023-07-29 21:07:47 +0000

    feat(web-app): add routes for back-end and fornt-end

--- apps/web-app/src/pages/User/hooks/useUserData.ts
+++ apps/web-app/src/pages/User/hooks/useUserData.ts
@@ -1,11 +1,12 @@
 import { axiosFetcher } from '@/config'
 import { type IApiResponse, type IUserApiResponse } from '@/models'
+import { backRoutes } from '@/routing'
 import { useParams } from 'react-router-dom'
 import useSWR from 'swr'
 
 export const useUserData = () => {
   const { userName } = useParams()
-  const { isLoading, data, error } = useSWR<IApiResponse<IUserApiResponse>>(`http://localhost:3000/user/username/${userName as string}`, axiosFetcher)
+  const { isLoading, data, error } = useSWR<IApiResponse<IUserApiResponse>>(backRoutes.Dynamic.user(userName as string), axiosFetcher)
 
   console.log({ data })
   return { userName, isLoading, data, error }
--- apps/web-app/src/routing/routes.model.ts
+++ apps/web-app/src/routing/routes.model.ts
@@ -1,17 +1,42 @@
-const BaseRoutes = {
-  user: '/'
-} as const
+const getFrontendRoutes = () => {
+  const BaseRoutes = {
+    user: '/'
+  } as const
 
-export const CStaticRoutes = {
-  index: '/',
-  login: '/login',
-  register: '/register',
-  user: BaseRoutes.user + ':userName',
-  notFound: '*'
-} as const
+  const Static = {
+    index: '/',
+    login: '/login',
+    register: '/register',
+    user: BaseRoutes.user + ':userName',
+    notFound: '*'
+  } as const
 
-export const CDynamicRoutes = {
-  user (id: string) {
-    return `${BaseRoutes.user}${id}`
+  const Dynamic = {
+    user (id: string) {
+      return `${BaseRoutes.user}${id}`
+    }
+  } as const
+
+  return { Static, Dynamic }
+}
+
+const getBackendRoutes = () => {
+  const Static = {
+    auth: {
+      signup: '/auth/sign-up',
+      signin: '/auth/sign-in'
+    }
+
   }
-} as const
+
+  const Dynamic = {
+    user (userName: string) {
+      return `/user/username/${userName}`
+    }
+  } as const
+
+  return { Static, Dynamic }
+}
+
+export const frontRoutes = getFrontendRoutes()
+export const backRoutes = getBackendRoutes()
