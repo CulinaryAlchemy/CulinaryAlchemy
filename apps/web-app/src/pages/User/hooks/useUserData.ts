c1df425ef64f15f86074386286ed30e57236db1a
Jes015 <jes.18.sin.15@gmail.com>
2023-07-31 17:38:38 +0000

    fix(web-app): change swr configuration
    add config for swr to the global directory and use swr config provider

--- apps/web-app/src/pages/User/hooks/useUserData.ts
+++ apps/web-app/src/pages/User/hooks/useUserData.ts
@@ -1,4 +1,3 @@
-import { axiosFetcher } from '@/config'
 import { type IApiResponse, type IUserApiResponse } from '@/models'
 import { backRoutes } from '@/routing'
 import { useParams } from 'react-router-dom'
@@ -6,8 +5,7 @@ import useSWR from 'swr'
 
 export const useUserData = () => {
   const { userName } = useParams()
-  const { isLoading, data, error } = useSWR<IApiResponse<IUserApiResponse>>(backRoutes.Dynamic.user(userName as string), axiosFetcher)
+  const { isLoading, data, error } = useSWR<IApiResponse<IUserApiResponse>, IApiResponse<IUserApiResponse>>(backRoutes.Dynamic.user(userName as string))
 
-  console.log({ data })
   return { userName, isLoading, data, error }
 }
