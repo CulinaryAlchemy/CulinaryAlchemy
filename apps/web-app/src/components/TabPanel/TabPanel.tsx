f48ffc6400548e7694955986ad356f1ea36c241a
Jes015 <jes.18.sin.15@gmail.com>
2023-07-31 22:35:50 +0000

    feat(web-app): add a loading component

--- apps/web-app/src/components/TabPanel/TabPanel.tsx
+++ apps/web-app/src/components/TabPanel/TabPanel.tsx
@@ -1,30 +1,41 @@
-import { Tweet } from '@/components'
-import { Typography } from '@mui/joy'
+import { Loading, Tweet } from '@/components'
+import { MessageLayout } from '@/layouts'
+import Box from '@mui/joy/Box'
 import DefaultUIPanel from '@mui/joy/TabPanel'
+import { useState } from 'react'
 
 interface IProps {
   value: string
 }
 export const TabPanel: React.FC<IProps> = ({ value }) => {
+  const [loading] = useState(true)
+
   return (
-        <DefaultUIPanel value={value}>
-            <Typography
-                level="h2"
-                component="div"
-                fontSize="lg"
-                mb={2}
-                textColor="text.primary"
-            >
-                <Tweet />
-                <Tweet />
-                <Tweet />
-                <Tweet />
-                <Tweet />
-                <Tweet />
-                <Tweet />
-                <Tweet />
-                <Tweet />
-            </Typography>
-        </DefaultUIPanel>
+    <DefaultUIPanel value={value}>
+      {
+        loading
+          ? <TabPanelLoading />
+          : <>
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+          </>
+      }
+
+    </DefaultUIPanel>
+  )
+}
+
+const TabPanelLoading = () => {
+  return (
+    <Box sx={{ position: 'relative', backgroundColor: 'var(--joy-palette-background-surface)', height: '10.5em' }}>
+      <MessageLayout><Loading size='md' /></MessageLayout>
+    </Box>
   )
 }
--- apps/web-app/src/pages/NotFound/NotFound.tsx
+++ apps/web-app/src/pages/NotFound/NotFound.tsx
@@ -1,16 +1,18 @@
-import { GlobalLayout } from '@/layouts'
+import { GlobalLayout, MessageLayout } from '@/layouts'
 import SentimentVeryDissatisfiedRoundedIcon from '@mui/icons-material/SentimentVeryDissatisfiedRounded'
-import Box from '@mui/joy/Box/'
+import Stack from '@mui/joy/Stack'
 import Typography from '@mui/joy/Typography/'
 import { metadata } from './config'
 
 const NotFound = () => {
   return (
     <GlobalLayout newTitle={metadata.title}>
-      <Box sx={{ height: '30vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-        <SentimentVeryDissatisfiedRoundedIcon sx={{ fontSize: '9em' }} />
-        <Typography level="h1" textTransform='uppercase'>Not Found</Typography>
-      </Box>
+      <MessageLayout>
+        <Stack direction='column' alignItems='center'>
+          <SentimentVeryDissatisfiedRoundedIcon sx={{ fontSize: '9em' }} />
+          <Typography level="h1" textTransform='uppercase'>Not Found</Typography>
+        </Stack>
+      </MessageLayout>
     </GlobalLayout>
   )
 }
