e9168a1214f7edeb7131b5baf2cda4f08a71856d
Jes015 <120581623+Jes015@users.noreply.github.com>
2023-08-17 04:58:30 +0000

    fix(web-app): commas and quotes errors #139
    close #139

--- apps/web-app/src/utils/errorMessages.util.ts
+++ apps/web-app/src/utils/errorMessages.util.ts
@@ -5,11 +5,11 @@ export const getValidationError = (code: number) => {
     200: 'OK',
     201: 'Created',
     400: 'Bad request or User registered',
-    401: 'Session expired or Not authorized'
+    401: 'Session expired or Not authorized',
     403: 'Wrong Email or Password',
     404: 'Not Found or Bad credentials',
     409: 'Username or email already exists',
-    503: 'Sever is down. Wait 2 minutes. thnks <3
+    503: 'Sever is down. Wait 2 minutes. thnks <3'
   }
 
   return httpCodes[code]
