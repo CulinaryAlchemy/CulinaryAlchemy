e3f114a6d314c9ab9823dcdf7758b973d38dbf5c
Huilensolis <xywvanz@gmail.com>
2023-09-25 16:16:35 -0300

    feat(server): fix cors config

--- apps/server/src/config/cors.ts
+++ apps/server/src/config/cors.ts
@@ -1,35 +1,28 @@
 import { CorsOptions } from 'cors';
 
 const corsConfig: CorsOptions = {
-	origin: (_origin, next) => {
-		return next(null, true);
+	origin: (origin, next) => {
+		if (!origin) {
+			return next(new Error('Rejected by cors. No origin detected'));
+		}
 
-		// if (!origin) {
-		// 	return next(
-		// 		new Error(
-		// 			'Rejected by cors, the is been an error on the cors configuration. origin value: ' +
-		// 				origin
-		// 		)
-		// 	);
-		// }
+		const whiteList = process.env.ALLOWED_ORIGIN_LIST;
 
-		// const whiteList = process.env.ALLOWED_ORIGIN_LIST;
+		if (!whiteList) {
+			return next(
+				new Error(
+					'No whiteList found in server variables. Check the .env and corsConfig to detect the error'
+				)
+			);
+		}
 
-		// if (!whiteList) {
-		// 	return next(
-		// 		new Error(
-		// 			'No whiteList found in server variables. Check the corsConfig to detect the error'
-		// 		)
-		// 	);
-		// }
+		for (const allowedOrigin of whiteList.split(' ')) {
+			if (origin?.startsWith(allowedOrigin)) {
+				return next(null, true);
+			}
+		}
 
-		// for (const allowedOrigin of whiteList.split(' ')) {
-		// 	if (origin?.startsWith(allowedOrigin)) {
-		// 		return next(null, true);
-		// 	}
-		// }
-
-		// return next(new Error('Rejected by cors, origin: ' + origin));
+		return next(new Error('Rejected by cors'));
 	},
 };
 export { corsConfig };
