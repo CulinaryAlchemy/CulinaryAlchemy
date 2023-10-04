fdfcdc56c28cee8e45f4ada0feb6b3f647624dae
Jes015 <jes.18.sin.15@gmail.com>
2023-10-04 16:15:59 -0500

    fix(web-app): carousel logic (#325)
    close #325 KAN-47

--- apps/web-app/src/components/Carousel/hooks/useCarousel.ts
+++ apps/web-app/src/components/Carousel/hooks/useCarousel.ts
@@ -1,57 +1,21 @@
 import { useState } from 'react'
-import { carouselRoutes } from '../routing'
 
 interface IParams {
   imageSources: string[]
-  carouselId: string
 }
-export const useCarousel = ({ imageSources, carouselId }: IParams) => {
+export const useCarousel = ({ imageSources }: IParams) => {
   const [imageIndex, setImageIndex] = useState(0)
 
   const nextImage = () => {
     const isTheLastImage = imageIndex === imageSources.length - 1
     if (isTheLastImage) return
-    setImageIndex((lastImageIndex) => {
-      const newImageIndex = lastImageIndex + 1
-
-      const element = document.getElementById(carouselRoutes.dynamic.getImageRoute(carouselId, String(newImageIndex)))
-
-      goToElement(element as HTMLElement)
-
-      return newImageIndex
-    })
+    setImageIndex((lastImageIndex) => lastImageIndex + 1)
   }
 
   const previousImage = () => {
     const isTheFirstImage = imageIndex === 0
     if (isTheFirstImage) return
-    setImageIndex((lastImageIndex) => {
-      const newImageIndex = lastImageIndex - 1
-
-      const element = document.getElementById(carouselRoutes.dynamic.getImageRoute(carouselId, String(newImageIndex)))
-      goToElement(element as HTMLElement)
-
-      return newImageIndex
-    })
-  }
-
-  const goToElement = (element: HTMLElement) => {
-    // @ts-expect-error firefox does not support this :(
-    if (element?.scrollIntoViewIfNeeded != null) {
-      // @ts-expect-error firefox does not support this :( - Firefox
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      element?.scrollIntoViewIfNeeded({ behavior: 'smooth' })
-    } else {
-      element?.scrollIntoView({
-        // @ts-expect-error firefox suckssssssss
-        top: element?.offsetParent?.offsetTop as HTMLElement,
-        // @ts-expect-error firefox suckssssssss
-        bottom: element?.offsetParent?.offsetTop as HTMLElement,
-        behavior: 'smooth',
-        block: 'nearest',
-        inline: 'start'
-      })
-    }
+    setImageIndex((lastImageIndex) => lastImageIndex - 1)
   }
 
   return { nextImage, previousImage, imageIndex }
