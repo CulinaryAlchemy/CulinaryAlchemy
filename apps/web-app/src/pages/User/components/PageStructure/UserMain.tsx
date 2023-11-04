c6acbbd7e94b2dfbb2e721ce5c50f02d28b6be7a
Jes015 <jes.18.sin.15@gmail.com>
2023-11-04 12:02:38 -0500

    refactor(web-app): remove tab navigation from user page (#411)
    close #411 KAN-100

--- apps/web-app/src/pages/User/components/PageStructure/UserMain.tsx
+++ apps/web-app/src/pages/User/components/PageStructure/UserMain.tsx
@@ -1,7 +1,7 @@
 
-import { Recipe, TabPanel, TabsPage } from '@/components'
+import { Recipe } from '@/components'
 import { MessageLayout } from '@/layouts'
-import { CTabsData } from '@/pages/User/models/UI/'
+import Box from '@mui/joy/Box'
 import Typography from '@mui/joy/Typography'
 
 interface IProps {
@@ -10,59 +10,42 @@ interface IProps {
 
 export const UserMain: React.FC<IProps> = ({ recipesIds }) => {
   return (
-    <main>
-      <TabsPage
-        defaultTab={CTabsData.recipes.name}
-        type='no-routing'
-        tabsData={Object.values(CTabsData)}
-        tabPanels={
-          <>
-            {Object.values(CTabsData).map((tabName) => (
-              <TabPanel routingBy='defaultUISystem' key={tabName.name} value={tabName.name} loading={false} >
-                {
-                  recipesIds[0] != null
-                    ? (
-                        recipesIds?.map((recipeId) => (
-                      <Recipe
-                        recipeId={recipeId.id ?? 0}
-                        key={recipeId.id}
-                        showStartCookingButton={false}
-                        styles={{
-                          cursor: 'pointer',
-                          content: {
-                            flexDirection: 'column',
-                            gap: '1em',
-                            carrousel: {
-                              maxWidth: '100%'
-                            },
-                            informationSection: {
-                              paddingInline: '1em'
-                            }
-                          }
-                        }}
-                      />
-                        ))
-                      )
-                    : (
-                    <MessageLayout>
-                      <Typography level='h3' sx={{ textTransform: 'uppercase' }}>No recipes found</Typography>
-                    </MessageLayout>
-                      )
-                }
-              </TabPanel>
-            ))}
-          </>
-        }
-        styles={{
-          background: 'theme',
-          direction: 'column',
-          tabs: {
-            names: {
-              direction: 'row'
-            }
-          }
-        }}
-      />
-    </main>
+    <Box
+      component='main'
+      sx={{
+        borderTop: '0.1em solid var(--joy-palette-neutral-outlinedBorder, var(--joy-palette-neutral-200, #D8D8DF));'
+      }}
+    >
+      {
+        recipesIds[0] != null
+          ? (
+              recipesIds?.map((recipeId) => (
+              <Recipe
+                recipeId={recipeId.id ?? 0}
+                key={recipeId.id}
+                showStartCookingButton={false}
+                styles={{
+                  cursor: 'pointer',
+                  content: {
+                    flexDirection: 'column',
+                    gap: '1em',
+                    carrousel: {
+                      maxWidth: '100%'
+                    },
+                    informationSection: {
+                      paddingInline: '1em'
+                    }
+                  }
+                }}
+              />
+              ))
+            )
+          : (
+            <MessageLayout>
+              <Typography level='h3' sx={{ textTransform: 'uppercase' }}>No recipes found</Typography>
+            </MessageLayout>
+            )
+      }
+    </Box>
   )
 }
